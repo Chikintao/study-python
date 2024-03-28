f85f66d679a4cf9019bbf64c851b5a04c61de7e1
chikintao <max.akh.on@gmail.com>
2024-03-28 16:40:03 +0200

    lesson4 listdict tuple

---
+++ lesson 4/listOfNumbers.py
@@ -0,0 +1,8 @@
+#   Завдання: Створіть список чисел. Додайте до списку числа 10 і 20, видаліть число 10 і виведіть отриманий список.
+
+numbers_list = [2,45,2,4]
+numbers_list.append(10)
+numbers_list.append(20)
+print(numbers_list)
+numbers_list.remove(10)
+print(numbers_list)
