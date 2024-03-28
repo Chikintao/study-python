fe95102c291c7523b290a3ab486182ba6e8643d9
chikintao <max.akh.on@gmail.com>
2024-03-28 05:23:16 +0200

    lesson1

---
+++ Lesson1/types.py
@@ -0,0 +1,19 @@
+#Створи змінні таких типів: integer, float, string, bool, list, tuple, dict, None
+
+age = 23
+name = "Max"
+weight = 104.6
+men = True
+hobbies = {"running", "art", "programming"} #list
+skills = ("js", "python", "html", "css") #tuple
+my_info = {"name":"Max", "age": 25, "city": "Kiev"} #dict
+work = None
+
+print(type(age))
+print(type(name))
+print(type(weight))
+print(type(men))
+print(type(hobbies))
+print(type(skills))
+print(type(my_info))
+print(type(work))
